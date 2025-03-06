2dd27abd0cc47f37fce946e4901ef365c6b3a427
Luiz Paes <luiz.rezende@baasic.com.br>
2025-03-06 15:29:36 -0300

    Initial commit: Setup project base with Vite and basic React structure

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier'
+    ],
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {
+      'prettier/prettier': 'error'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    env: {
+      browser: true,
+      es6: true,
+      node: true
+    }
+  };
+  
